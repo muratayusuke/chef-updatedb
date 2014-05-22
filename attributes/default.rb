ed7853b475d1afd1c22b5679356941b068b09bec
Luke Waite <waitel@me.com>
2014-05-22 09:12:48 -0400

    Remove .git and .svn from default prunepaths
    This setting would only prune from /.git and /.svn
    http://linux.die.net/man/5/updatedb.conf

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default['updatedb']['prune_bind_mounts'] = 'yes'
 default['updatedb']['prunenames'] = '.git .bzr .hg .svn'
-default['updatedb']['prunepaths'] = '/tmp /var/spool /media /home/.ecryptfs .git .svn'
+default['updatedb']['prunepaths'] = '/tmp /var/spool /media /home/.ecryptfs'
 default['updatedb']['prunefs'] = 'NFS nfs nfs4 rpc_pipefs afs binfmt_misc proc smbfs autofs iso9660 ncpfs coda devpts ftpfs devfs mfs shfs sysfs cifs lustre_lite tmpfs usbfs udf fuse.glusterfs fuse.sshfs curlftpfs ecryptfs fusesmb devtmpfs'
